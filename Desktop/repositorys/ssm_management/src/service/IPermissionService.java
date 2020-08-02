23ccf0529bcf6a7b596d4e5d6305fbdab1c65ab7
李琼 <2789231577@qq.com>
2020-08-02 11:25:37 +0800

    ssm-后台管理系统

---
+++ Desktop/repositorys/ssm_management/src/service/IPermissionService.java
@@ -0,0 +1,16 @@
+package service;
+
+import domain.Permission;
+
+import java.util.List;
+
+public interface IPermissionService {
+
+    public List<Permission> findAll() throws Exception;
+
+    void save(Permission permission) throws Exception;
+
+    Permission findById(String id) throws Exception;
+
+    void deleteById(String id) throws Exception;
+}
---
+++ Desktop/repositorys/ssm_management/src/service/IUserService.java
@@ -0,0 +1,19 @@
+package service;
+
+import domain.Role;
+import domain.UserInfo;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+
+public interface IUserService extends UserDetailsService {
+    List<UserInfo> findAll() throws Exception;
+
+    void save(UserInfo userInfo) throws Exception;
+
+    UserInfo findById(String id) throws Exception;
+
+    List<Role> findOtherRoles(String userId) throws Exception;
+
+    void addRoleToUser(String userId, String[] roleIds);
+}
